c9e7793aba8a5820f6eb4f0bccf0fd505ca6fa11
Abduzhalilova Mukhabbat <148844893+Mukhabbat01@users.noreply.github.com>
2024-06-15 21:32:33 +0900

    Update ProductSkinType.java

--- src/main/java/com/crystalskin/domain/ProductSkinType.java
+++ src/main/java/com/crystalskin/domain/ProductSkinType.java
@@ -1,42 +1,42 @@
-package com.crystalskin.domain;
+// package com.crystalskin.domain;
 
-import java.io.Serializable;
+// import java.io.Serializable;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Embeddable;
-import jakarta.persistence.EmbeddedId;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+// import jakarta.persistence.CascadeType;
+// import jakarta.persistence.Embeddable;
+// import jakarta.persistence.EmbeddedId;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.FetchType;
+// import jakarta.persistence.JoinColumn;
+// import jakarta.persistence.ManyToOne;
+// import lombok.AllArgsConstructor;
+// import lombok.Getter;
+// import lombok.NoArgsConstructor;
+// import lombok.Setter;
 
-@Entity
-@Getter
-@Setter
-public class ProductSkinType {
+// @Entity
+// @Getter
+// @Setter
+// public class ProductSkinType {
 	
-	@EmbeddedId
-	PrdSkinListsId prdSkinListsId;
+// 	@EmbeddedId
+// 	PrdSkinListsId prdSkinListsId;
 
 
 
-	@Embeddable
-	@Getter
-	@Setter
+// 	@Embeddable
+// 	@Getter
+// 	@Setter
 
-	@NoArgsConstructor
-	@AllArgsConstructor
-	public static class PrdSkinListsId implements Serializable {
-		@ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-		@JoinColumn(name = "skin_id")
-		private SkinCat skinCat;
+// 	@NoArgsConstructor
+// 	@AllArgsConstructor
+// 	public static class PrdSkinListsId implements Serializable {
+// 		@ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+// 		@JoinColumn(name = "skin_id")
+// 		private SkinCat skinCat;
 
-		@ManyToOne(fetch = FetchType.LAZY)
-		@JoinColumn(name = "prd_id")
-		private Product product;
-	}
-}
+// 		@ManyToOne(fetch = FetchType.LAZY)
+// 		@JoinColumn(name = "prd_id")
+// 		private Product product;
+// 	}
+// }
