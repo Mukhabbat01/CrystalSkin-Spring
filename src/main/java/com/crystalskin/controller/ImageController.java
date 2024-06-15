c695fde5c9acc7a115c90800f3b41b035dc03b22
Abduzhalilova Mukhabbat <148844893+Mukhabbat01@users.noreply.github.com>
2024-06-15 21:32:58 +0900

    Update ImageController.java

--- src/main/java/com/crystalskin/controller/ImageController.java
+++ src/main/java/com/crystalskin/controller/ImageController.java
@@ -1,29 +1,29 @@
-package com.crystalskin.controller;
+// package com.crystalskin.controller;
 
-import java.io.IOException;
+// import java.io.IOException;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RequestParam;
+// import org.springframework.web.bind.annotation.RestController;
 
-import com.crystalskin.service.FileService;
+// import com.crystalskin.service.FileService;
 
-import lombok.RequiredArgsConstructor;
+// import lombok.RequiredArgsConstructor;
 
-@RestController
-@RequiredArgsConstructor
-public class ImageController {
+// @RestController
+// @RequiredArgsConstructor
+// public class ImageController {
 
-	private final FileService fileService;
-//	
-//	@GetMapping("/crawl-images")
-//    public String crawlImages(@RequestParam String url) {
-//        try {
-//            fileService.crawlingAndSaveImages(url);
-//            return "Images crawled and saved successfully!";
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//            return "Failed to crawl images: " + e.getMessage();
-//        }
-//	}
-}
+// 	private final FileService fileService;
+// //	
+// //	@GetMapping("/crawl-images")
+// //    public String crawlImages(@RequestParam String url) {
+// //        try {
+// //            fileService.crawlingAndSaveImages(url);
+// //            return "Images crawled and saved successfully!";
+// //        } catch (IOException e) {
+// //            e.printStackTrace();
+// //            return "Failed to crawl images: " + e.getMessage();
+// //        }
+// //	}
+// }
